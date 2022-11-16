5f3bfd4829ef9afc6deb5adf4f4a0829804bd942
Viroshen <69042322+Viroshen@users.noreply.github.com>
2022-11-16 17:06:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function search_products() {
         }
     }
 }
+
+var ul = document.getElementById("cart-items")
+if (ul.childElementCount == 0) {
+    var li = document.createElement('li')
+    li.setAttribute('id', 'empty')
+    li.appendChild(document.createTextNode('Cart is empty!'))
+    ul.appendChild(li)
+}
+
+function addToCart(id) {
+    var empty = document.getElementById('empty')
+    if (empty != null) {
+        empty.parentNode.removeChild(empty)
+    }
+
+    // var newItem = document.getElementById(id);
+    var li = document.createElement('li')
+    li.appendChild(document.createTextNode(id))
+    ul.appendChild(li)
+}
