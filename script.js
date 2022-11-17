a961570f856043fbd58ab2ad09a6c114eb8ee393
Viroshen <69042322+Viroshen@users.noreply.github.com>
2022-11-17 17:48:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,11 +14,32 @@ function search_products() {
 }
 
 var ul = document.getElementById("cart-items")
-if (ul.childElementCount == 0) {
-    var li = document.createElement('li')
-    li.setAttribute('id', 'empty')
-    li.appendChild(document.createTextNode('Cart is empty!'))
-    ul.appendChild(li)
+
+function isCartEmpty() {
+    if (ul.childElementCount == 0) {
+        var li = document.createElement('li')
+        li.setAttribute('id', 'empty')
+        li.appendChild(document.createTextNode('Cart is empty!'))
+        ul.appendChild(li)
+    }
+}
+
+function generateID() {
+    var newID = ''
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var charactersLength = characters.length
+    var itemIDs = document.getElementsByClassName('cart-list-item')
+
+    for (j = 0; j < 5; j++) {
+        newID += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    
+    for (i = 0; i < itemIDs.length; i++) {
+        if (newID == itemIDs[i].id) {
+            generateID()
+        }
+    }
+    return (newID)
 }
 
 function addToCart(id) {
@@ -27,8 +48,25 @@ function addToCart(id) {
         empty.parentNode.removeChild(empty)
     }
 
-    // var newItem = document.getElementById(id);
     var li = document.createElement('li')
-    li.appendChild(document.createTextNode(id))
+    li.setAttribute('class', 'cart-list-item')
+
+    // Generate and assign unique id# for each list item //
+    var itemID = generateID()
+    li.setAttribute('id', 'item#' + itemID)
+    li.innerHTML = id + '<span class="remove-item" onClick="removeItem(this.parentNode.id)">x</span>'
     ul.appendChild(li)
+}
+
+function removeItem(id) {
+    var item = document.getElementById(id)
+    item.parentNode.removeChild(item)
+    isCartEmpty()
+}
+
+function clearCart() {
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+    }
+    isCartEmpty()
 }
